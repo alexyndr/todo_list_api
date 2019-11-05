1e1cba2738a28174a87220ee1eaa22e40f39951f
alexyndr belik <alexyndr@gmail.com>
2019-11-05 16:34:49 +0200

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+def create_project
+  Project.create(name: 'project name', user_id: User.first.id)
+end
+
+def create_task
+  Task.create(name: 'task name', project_id: Project.first.id)
+end
+
+def create_comment
+  Comment.create(body: 'comment name', task_id: Task.first.id)
+end
+
+User.create(email: 'alex@alex.com', password: 'password', password_confirmation: 'password')
+
+1.times { create_project }
+1.times { create_task }
+1.times { create_comment }
